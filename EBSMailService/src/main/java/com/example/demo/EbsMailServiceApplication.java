a10d8b14ccb78678a9b8c8b2f1394b23a0c611cf
saurabhanimje <nimjesaurabh1998@gmail.com>
2024-12-16 14:45:59 +0530

    Mail Structure commited

---
+++ EBSMailService/src/main/java/com/example/demo/EbsMailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbsMailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbsMailServiceApplication.class, args);
+	}
+
+}
---
+++ EBSMailService/src/main/java/com/example/demo/serviceimpl/MailServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.service.MailService;
+
+@Service
+public class MailServiceImpl implements MailService
+{
+
+}
